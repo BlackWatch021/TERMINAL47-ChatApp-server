b37cd528ce424f35431c9c8afab334a10c82458d
Himanshu Negi <himanshu.21negi@gmail.com>
2026-02-07 22:23:03 +0530

    Socket.io initialized

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
-import express from "express"
+import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
-
+const port = process.env.PORT || 8001;
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 const server = createServer(app);
 const io = new Server(server);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-    res.status(200).json("Hello World!");
+  res.status(200).json("Hello World!");
 });
 
-const port = process.env.PORT || 8001;
+io.on("connection", (socket) => {
+  console.log("A user connected");
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
